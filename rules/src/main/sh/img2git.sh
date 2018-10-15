111068b9fb1910d217237fdd8515bc24e8cb8575
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2018-10-15 22:03:54 +0200

    import(tools): import conversion scripts from palermo repo

---
+++ rules/src/main/sh/img2git.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+script_dir=$(dirname "$0")
+page_names=()
+dir_name=../../..
+base_dir="$script_dir/$dir_name"
+force=
+
+function usage {
+	echo "usage: $0 ((-p|--pagename) page_name)+ (-d|--dirname) dir_name [-f|--force]"
+	echo "usage: $0 (-h|--help)"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --pagename )	shift
+				page_names+=("$1")
+                                ;;
+        -d | --dirname )	shift
+				base_dir="$1"
+                                ;;
+	-f | --force )		force=1
+				;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+if [ ${#page_names[@]} -eq 0 ]; then
+	echo "Missing page_name attribute" >&2
+	usage
+	exit
+fi
+if [ -z "$dir_name" ]; then
+	echo "Missing dir_name attribute" >&2
+	usage
+	exit
+fi
+base_url="http://j2s-pendragon.wikia.com/wiki"
+wiki_sub_dir="src/main/wiki"
+img_sub_dir="src/main/img"
+work_subdir="target/work"
+
+tab=$'\t'
+
+# Import images list
+
+work_dir="$base_dir/$work_subdir"
+mkdir -p "$work_dir"
+images_file_html="$work_dir/images.html"
+if [ "$force" -o  \! -f "$images_file_html" ]; then
+	echo "Loading images list..."
+	curl "$base_url/Special:Images" -o - > "$images_file_html"
+fi
+images_file_xml="$work_dir/images.xml"
+if [ "$force" -o  \! -f "$images_file_xml" ]; then
+	echo "Converting images list to XML..."
+	tidy -quiet -numeric -asxml -wrap 0 -o "$images_file_xml" "$images_file_html"
+fi
+images_file_data="$work_dir/images.data"
+echo "Extracting data from images list..."
+if [ "$force" -o  \! -f "$images_file_data" ]; then
+	xpathmatch='//_:div[@class="wikia-gallery-item"]'
+	xpathvalue='concat(descendant::_:a[@class="wikia-gallery-item-posted"], "'"$tab"'", descendant::_:i, "'"$tab"'", descendant::_:img/@data-image-key)'
+	xml sel -t -m "$xpathmatch" -v "$xpathvalue" -n "$images_file_xml" | perl -CIO -pe '
+		BEGIN { use Time::Piece; };
+		my($referrer, $date, $image) = split "\t";
+		$t = Time::Piece->strptime($date, "%B %d, %Y");
+		$_ = join("\t", ($referrer, $t->strftime("%Y-%m-%d"), $image))
+		' > "$images_file_data"
+	echo Found $(wc -l < "$images_file_data") images
+fi
+
+for page_name in "${page_names[@]}"; do
+
+echo "* Processing page $page_name"
+mkdir -p "$base_dir/$img_sub_dir"
+
+git co wikia/images
+grep -p "^$page_name"'\t' "$images_file_data" | \
+sort -k2 | \
+while read image_record; do
+	image=$(echo "$image_record" | awk 'BEGIN {FS="\t"} { print $3 }' )
+	echo "  - Processing $image for $page_name..."
+	history_image_html="$work_dir/File:$image.html"
+	if [ "$force" -o  \! -f "$history_image_html" ]; then
+		echo "Loading history for $image..."
+		curl "$base_url/File:$image" -o - > "$history_image_html"
+	fi
+	history_image_xml="$work_dir/File:$image.xml"
+	if [ "$force" -o  \! -f "$history_image_xml" ]; then
+		echo "Converting history for $image to XML..."
+		tidy -quiet -numeric -asxml -wrap 0 -o "$history_image_xml" "$history_image_html"
+	fi
+	history_image_data="$work_dir/File:$image.data"
+	if [ "$force" -o  \! -f "$history_image_data" ]; then
+		echo "Extracting data from history for $image..."
+		xpathmatch='//_:td[text()="current"]/following-sibling::_:td[position()=1]'
+		xpathvalue='.'
+		xml sel -t -m "$xpathmatch" -v "$xpathvalue" -n "$history_image_xml" | perl -CIO -pe '
+		BEGIN { use Time::Piece; };
+		chomp;
+		$t = Time::Piece->strptime($_, "%H:%M, %B %d, %Y"); # TODO: UTC?
+		$_ = $t->strftime("%Y-%m-%dT%H:%MZ")
+		' > "$history_image_data"
+	fi
+	image_file="$base_dir/$img_sub_dir/$image"
+	if [ "$force" -o  \! -f "$image_file" -o \! -s "$image_file" ]; then
+		xpathmatch='//_:div[@class="fullImageLink"]/_:a'
+		xpathvalue='@href'
+		image_url=$(xml sel -t -m "$xpathmatch" -v "$xpathvalue" -n "$history_image_xml")
+		if [ "$image_url" ]; then
+			echo "$image_url"
+			curl "$image_url" -o "$image_file"
+		else
+			echo "URL not found for $image, skipping."
+		fi
+	fi
+	if [ -s "$image_file" ]; then
+		git add "$image_file"
+		#timestamp=$(cat "$history_image_data")
+		timestamp=$(cat "$history_image_data")"Z"
+		git commit --date="${timestamp}" -m "imported image: $image"
+		git log -n 1
+	else
+		echo "$image missing, skipping."
+	fi
+done
+
+done
+
+git checkout master
+git merge --no-edit wikia/images
+
---
+++ rules/src/main/sh/pg2git.sh
@@ -0,0 +1,119 @@
+#!/bin/sh
+
+infile="$1"
+if [ \! -s "$infile" ]; then
+	echo Missing infile argument, aborting.
+	exit 1
+fi
+
+script_dir=$(dirname "$0")
+dir_name=../../..
+base_dir="$script_dir/$dir_name"
+wiki_sub_dir="src/main/wiki"
+adoc_sub_dir="src/main/adoc"
+work_subdir="target/work"
+ns=http://www.mediawiki.org/xml/export-0.6/
+
+page_title=$( xmlstarlet sel -T -N w=$ns \
+	-t -m "//w:page" -v "w:title" \
+	"$infile" )
+revisions=($( xmlstarlet sel -T -N w=$ns \
+		-t -m '//w:revision' -v 'w:id' --nl \
+		"$infile" ))
+
+echo Found ${#revisions[@]} revisions
+
+mkdir -p "$base_dir/$wiki_sub_dir"
+wiki_page_path="$base_dir/$wiki_sub_dir/${page_title}.wiki"
+mkdir -p "$base_dir/$adoc_sub_dir"
+adoc_page_path="$base_dir/$adoc_sub_dir/${page_title}.adoc"
+work_dir="$base_dir/$work_subdir/$page_title"
+mkdir -p "$work_dir"
+git stash save "Saved before importing files"; git stash apply
+git checkout master
+git reset --hard import/code
+git branch -D wikia/pages
+git branch -D wikia/images
+git branch -D adoc
+git gc
+git branch wikia/pages
+git branch wikia/images
+git branch adoc
+for revision in ${revisions[@]}; do
+	git checkout wikia/pages
+	xmlstarlet sel -T -N w=$ns -E utf-8 \
+		-t -m "//w:revision[w:id='$revision']" -v 'w:text' \
+		"$infile" > "$wiki_page_path"
+	timestamp=$( xmlstarlet sel -T -N w=$ns \
+		-t -m "//w:revision[w:id='$revision']" -v 'w:timestamp' \
+		"$infile" )
+	comment=$( xmlstarlet sel -T -N w=$ns \
+		-t -m "//w:revision[w:id='$revision']" -v 'w:comment' \
+		"$infile" |
+		perl -pe 's#/\*\s+(.*)\s+\*?/#(\1):#g' )
+	echo "$revision - $timestamp : $comment"
+	git add "$wiki_page_path"
+	git commit --date="$timestamp" -m "$comment"
+	git log -n 1
+	#cp "$wiki_page_path" "$work_dir"
+	git checkout adoc
+	git merge --no-commit wikia/pages
+	perl -pe '
+		s:^'"'''"'(.*?)'"'''"'<br.*?/>$:$1\n:; # fix hardcoded document title
+		s:^<h2.*?>(.*?)</h2>$:==$1==:; # fix hardcoded heading
+		s:^=(.*)=$:$1:; # promote all headers
+		s:^<div\s+style="page-break-after\:\s+always"></div>$:\n>> PAGEBREAK HERE <<\n:; # remember hardcoded page break
+		s:<s>(.*?)</s>:%%s%$1%/s%%:g; # remember strike-through
+		s:(?<!'\'')'\''([^ '\'']+?)'\''(?!'\''):%%'\''%$1%'\''%%:g; # remember single quoted words
+		s#[\|\!](:?(r)ow|(c)ol)span="(\d+)".*?\|#$&%%$2$3$4%%#g; # remember colspan/rowspan
+		s:<br>:%%br%%:g; # remember line breaks
+		s:&beta;:%%beta%%:g; # remember beta character
+		s:\|thumb\]\]:$&%%thumb%%:g; # remember thumb images
+	' "$wiki_page_path" | \
+	pandoc -f mediawiki -t asciidoc --toc | \
+		perl -pe 'BEGIN {
+			use utf8;
+			use Text::Unidecode;
+			use Encode "decode";
+			}
+			($. == 1) and s{$}{
+=======
+Arnauld Van Muysewinkel <arnauldvm\@gmail.com>'"
+:revnumber: W$revision
+:revdate: ${timestamp:0:10}
+//:revremark: $comment"'
+:doctype: article
+:lang: fr
+:encoding: utf8
+:toc:
+:toc-placement: manual
+:toclevels: 4
+:toc-title: Contenu
+//:numbered:
+:imagesdir: ../img
+//:data-uri: // This corrupts some images because of a bug in base64 encoding, see https://github.com/asciidoc/asciidoc/issues/98 and https://groups.google.com/d/topic/asciidoc/pC22vFTCxTc/discussion
+:br: pass:[<br>]
+:beta: pass:[&beta;]
+};
+			s/>> PAGEBREAK HERE <</<<<\ntoc::[]\n<<</; # fix hardcoded page break
+			s/^\[\[.*?\]\]$/unidecode(decode "UTF-8", $&)/e; # fix identifiers with accents
+			s:%%s%(.*?)%/s%%:[line-through]#$1#:g; # fix strike-through
+			s:%%'\''%([^ '\'']+?)%'\''%%:\\'\''$1'\'':g; # fix single quoted words
+			s:\|%%c(\d+)%%:$1+|:g; # fix colspan
+			s:\|%%r(\d+)%%:.$1+|:g; # fix rowspan
+			s:%%br%%:{br}:g; # fix line breaks
+			s:%%beta%%:{beta}:g; # fix beta character
+			s/(image:.*?\[)(.*?),/\1"\2",/g; # fix images alt attribute
+			s:\]%%thumb%%:,width=180]:g; # fix thumb images
+		' > "$adoc_page_path"
+	git add "$adoc_page_path"
+	git commit --date="$timestamp" -m "convert: $comment"
+	git log -n 1
+done
+git checkout master
+git merge --no-edit adoc
+
+src/main/sh/img2git.sh -p ${page_title}
+
+asciidoc $adoc_page_path
+
